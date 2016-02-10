2b55e31766a99b5a41d5c81390f2dd7acccbea64
Shadybot <jsylvan@gmail.com>
2016-02-09 17:34:55 -0800

    Update InsectDamage.js
    Added the beginning of the KinSkill variable to determine the raw modifier that comes from the kinsect form.

--- InsectDamage.js
+++ InsectDamage.js
@@ -1,6 +1,7 @@
 $(document).on('click', 'input:button', function(){
 	/* Find the highest element value. */
-	Var TopElem = Math.max(insect.attributeStatusList.F.status,insect.attributeStatusList.W.status,insect.attributeStatusList.T.status,insect.attributeStatusList.I.status,insect.attributeStatusList.D.status)
+	var TopElem = Math.max(insect.attributeStatusList.F.status,insect.attributeStatusList.W.status,insect.attributeStatusList.T.status,insect.attributeStatusList.I.status,insect.attributeStatusList.D.status)
+	var KinSkill
 	/* find the relevant skill modifier
  SKILLS
 ATK Up S: 1.06
@@ -9,6 +10,8 @@ ATK Up L: 1.14
 All Up S: 1.04
 All Up M: 1.08
 All Up L: 1.10 */
+	/* console.log(KinSkill) */
+
 	/* Regular Raw */ 
 	$('#RegRaw').text(((insect.attributeStatusList.P.status - 15) / 9) * 10 * 0.45)
 	/* Regular Element*/
