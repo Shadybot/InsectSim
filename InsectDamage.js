9fe3791a46f08a1c4c46bde0118eb54fa7be0330
Shadybot <jsylvan@gmail.com>
2016-02-10 01:39:27 -0800

    Update InsectDamage.js
    Swapped the double arrays and a for loop for a single object.

--- InsectDamage.js
+++ InsectDamage.js
@@ -3,22 +3,30 @@ $(document).on('click', 'input:button', function(){
 	var TopElem = Math.max(insect.attributeStatusList.F.status,insect.attributeStatusList.W.status,insect.attributeStatusList.T.status,insect.attributeStatusList.I.status,insect.attributeStatusList.D.status)
 
 	/* find the relevant skill modifier */
-	var KinSkill
-	var KinType = ["(ATK)", "(ATK/Rec)", "(ATK+)", "(ATK+SPD)", "(ATK+STA)", "(BAL)", "(BAL+)", "(BAL++)", "(STA)", "(STA+)", "(STA/Rec)", "(SPD)", "(SPD+)", "(STA+SPD)", "(SPD/Rec)", "(New)"]
-	var KinTypeBonus = [1.06, 1.12, 1.14, 1.14, 1.14, 1.04, 1.08, 1.1, 1, 1, 1, 1, 1, 1, 1, 1]
-	for (index = 0; index < KinType.length; index++) {
-		If (insect.insectTypeHolder.insectType.shortName == KinType[index]) {
-			KinSkill = KinTypeBonus[index];
-		}
-	}	
-	console.log(KinSkill) 
-	console.log(insect.insectTypeHolder.insectType.shortName)
+	var KinSkill = {
+		"(ATK)": 1.06, 
+		"(ATK/Rec)": 1.12, 
+		"(ATK+)": 1.14, 
+		"(ATK+SPD)": 1.14, 
+		"(ATK+STA)": 1.14, 
+		"(BAL)": 1.04, 
+		"(BAL+)": 1.08, 
+		"(BAL++)": 1.1, 
+		"(STA)": 1, 
+		"(STA+)": 1, 
+		"(STA/Rec)": 1, 
+		"(SPD)": 1, 
+		"(SPD+)": 1, 
+		"(STA+SPD)": 1, 
+		"(SPD/Rec)": 1, 
+		"(New)": 1
+	}
 	/* Regular Raw */ 
-	$('#RegRaw').text(((insect.attributeStatusList.P.status - 15) / 9) * 10 * 0.45)
+	$('#RegRaw').text(+(((insect.attributeStatusList.P.status - 15) / 9) * 10 * 0.45 * KinSkill[insect.insectTypeHolder.insectType.shortName]).toFixed(2))
 	/* Regular Element*/
 	$('#RegElem').text(TopElem * 2.5)
 	/* Charged Raw */
-	$('#ChargeRaw').text(((insect.attributeStatusList.P.status - 15) / 9) * 10 * 0.8)
+	$('#ChargeRaw').text(+(((insect.attributeStatusList.P.status - 15) / 9) * 10 * 0.8 * KinSkill[insect.insectTypeHolder.insectType.shortName]).toFixed(2))
 	/* Charged Element */
 	$('#ChargeElem').text(TopElem * 2.5 * 1.5)
 });
