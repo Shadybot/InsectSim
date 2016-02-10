97e192e31ffbec167fb0c4cd90c2a12661997985
Shadybot <jsylvan@gmail.com>
2016-02-10 01:07:22 -0800

    Update InsectDamage.js

--- InsectDamage.js
+++ InsectDamage.js
@@ -1,17 +1,18 @@
 $(document).on('click', 'input:button', function(){
 	/* Find the highest element value. */
 	var TopElem = Math.max(insect.attributeStatusList.F.status,insect.attributeStatusList.W.status,insect.attributeStatusList.T.status,insect.attributeStatusList.I.status,insect.attributeStatusList.D.status)
-	var KinSkill
-	/* find the relevant skill modifier
- SKILLS
-ATK Up S: 1.06
-ATK Up M: 1.12
-ATK Up L: 1.14
-All Up S: 1.04
-All Up M: 1.08
-All Up L: 1.10 */
-	/* console.log(KinSkill) */
 
+	/* find the relevant skill modifier */
+	var KinSkill
+	var KinType = ["(ATK)", "(ATK/Rec)", "(ATK+)", "(ATK+SPD)", "(ATK+STA)", "(BAL)", "(BAL+)", "(BAL++)", "(STA)", "(STA+)", "(STA/Rec)", "(SPD)", "(SPD+)", "(STA+SPD)", "(SPD/Rec)", "(New)"]
+	var KinTypeBonus = [1.06, 1.12, 1.14, 1.14, 1.14, 1.04, 1.08, 1.1, 1, 1, 1, 1, 1, 1, 1, 1]
+	for (index = 0; index < KinType.length; index++) {
+		If (insect.insectTypeHolder.insectType.shortName == KinType[index]) {
+			KinSkill = KinTypeBonus[index];
+		}
+	}	
+	console.log(KinSkill) 
+	console.log(insect.insectTypeHolder.insectType.shortName)
 	/* Regular Raw */ 
 	$('#RegRaw').text(((insect.attributeStatusList.P.status - 15) / 9) * 10 * 0.45)
 	/* Regular Element*/
